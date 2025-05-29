3359fa0dabd7fee455eb6625d6565064517a97fb
SMITH MOGAMAT ZUHAYR <126153069+kxngzero329@users.noreply.github.com>
2025-05-29 19:03:28 +0200

    Add files via upload

---
+++ js exercise 3/arrays.js
@@ -0,0 +1,15 @@
+// TODO: Create an empty array called colors
+let colors = [];
+// TODO: Add three colors to the array using push()
+colors.push("red");
+colors.push("green");
+colors.push("blue"); //output: ["red", "green", "blue"]
+// TODO: Create another array called numbers with five numbers
+let numbers = [1, 2, 3, 4, 5]; // output: [1, 2, 3, 4, 5]
+// TODO: Remove the last color from the colors array
+colors.pop(); // output: ["red", "green"]
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("yellow"); // output: ["yellow", "red", "green"]
+// TODO: Print the length of both arrays
+console.log("Length of colors array:", colors.length); // output: 3
+console.log("Length of numbers array:", numbers.length); // output: 5
